d480830b1ff3fc18207ec2dc91a5f355f671e40e
Michael Markey <michaelamarkey@gmail.com>
2020-11-03 11:21:20 +0900

    tsc and jest setup with binary search

---
+++ algs/binary-search/binary-search.ts
@@ -0,0 +1,21 @@
+function binarySearch(searchVal: number, arr: number[], start = 0, end = arr.length - 1) {
+  const mid = Math.floor((start + end) / 2)
+
+  if (searchVal === arr[mid]) {
+    return mid
+  } else {
+    if (searchVal > arr[mid]) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+
+    if (end < start) {
+      return null
+    } else {
+      return binarySearch(searchVal, arr, start, end)
+    }
+  }
+}
+
+module.exports = binarySearch
